2928633adf22b4cb28e179ed44146a67168f6ab1
FrançoisB <38584907+voigi@users.noreply.github.com>
2022-07-02 18:14:38 +0200

    hello world
    Premier code avec Java

---
+++ HelloWorld/Helloworld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package HelloWorld;
+
+/**
+ * @author voigi
+ *
+ */
+public class Helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+
+	}
+
+}
