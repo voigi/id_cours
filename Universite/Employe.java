9c1d512736a6e143f081cb2a2c76ff875b658709
FrançoisB <38584907+voigi@users.noreply.github.com>
2022-07-10 17:11:36 +0200

    Exercice sur l'héritage de classes
    Ici Etudiant hérite de Personne comme Employé
    Professeur est une personne qui hérite de Employé.
    Le fichier Personnel lance l'affichage de tout ce beau monde

---
+++ Universite/Employe.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package Universite;
+
+/**
+ * @author Stagiaire
+ *
+ */
+public class Employe extends Personne {
+	
+	private int Salaire ;
+
+	/**
+	 * 
+	 */
+	public Employe() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param id
+	 * @param nom
+	 * @param prenom
+	 */
+	public Employe(int id, String nom, String prenom,int Salaire) {
+		super(id, nom, prenom);
+		this.Salaire = Salaire;
+	}
+
+	/**
+	 * @return the salaire
+	 */
+	public int getSalaire() {
+		return Salaire;
+	}
+
+	/**
+	 * @param salaire the salaire to set
+	 */
+	public void setSalaire(int salaire) {
+		Salaire = salaire;
+	}
+
+	@Override
+	public String toString() {
+		return "Employe "  + getId() +": "+"Je m'apelle "+ getPrenom()+ getNom()+" je suis payé "+ getSalaire()+" euros";
+	}
+	
+	
+
+}
